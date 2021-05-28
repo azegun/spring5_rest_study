fda56f15fb03007988aef0b3a3bb7ed97c69d317
KImSangGeon <tkdrjs7@naver.com>
2021-05-28 09:45:21 +0900

    Mybatis연결

---
+++ src/main/java/spring5_rest_study/config/ContextRoot.java
@@ -0,0 +1,13 @@
+package spring5_rest_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({ContextDataSource.class, ContextSqlSession.class})
+@ComponentScan(basePackages = {"spring5_rest_study.controller",																		
+																		"spring5_rest_study.service",
+																		"spring5_rest_study.common"
+																		})
+public class ContextRoot {} 
